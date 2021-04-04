baf10cb7890641933acf663db5de54420259f8dc
Dimitrey1978 <Dimitrey@mail.ru>
2021-04-04 20:26:34 +0300

    Добавьте файлы проекта.

---
+++ 008-structures-dop/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _008_structures_dop
+{
+    class Program
+    {
+        static void Main()
+        {
+
+
+            Console.WriteLine("Введите день рождения в формате: дд/мм/гггг");
+            string drVvod = Console.ReadLine();
+            DateTime now = DateTime.Now;
+            DateTime dr = DateTime.Parse(drVvod);
+
+            if (dr.DayOfYear > now.DayOfYear) //др еще не наступил                        
+                Console.WriteLine($"До дня рождения осталось: {dr.DayOfYear - now.DayOfYear - 1} дней");
+
+            else // др уже прошел                       
+                Console.WriteLine($"До дня рождения осталось: {365 - (now.DayOfYear - dr.DayOfYear) - 1} дней");
+
+
+
+            TimeSpan projito = now - dr;
+
+            Console.WriteLine($"Вы прожили {projito.Days} дней с даты рождения");
+
+        }
+    }
+}
